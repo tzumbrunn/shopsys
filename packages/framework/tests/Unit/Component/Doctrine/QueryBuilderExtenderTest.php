67a66631ffe9abf72cc9f315673994211e0de94f
David Stritezsky <david.stritezsky@shopsys.com>
2020-03-02 15:16:28 +0100

    fixup! used sprintf() for used variables in string

--- packages/framework/tests/Unit/Component/Doctrine/QueryBuilderExtenderTest.php
+++ packages/framework/tests/Unit/Component/Doctrine/QueryBuilderExtenderTest.php
@@ -66,7 +66,8 @@ class QueryBuilderExtenderTest extends TestCase
                 Category::class,
                 $expectedJoinedEntity
             ),
-            $dql);
+            $dql
+        );
     }
 
     /**

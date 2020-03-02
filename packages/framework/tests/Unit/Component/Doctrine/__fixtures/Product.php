f530d2faab137d86c48a7151202cb223aa73c85c
David Stritezsky <david.stritezsky@shopsys.com>
2020-03-02 14:54:27 +0100

    used extended fixture class in tests

---
+++ packages/framework/tests/Unit/Component/Doctrine/__fixtures/Product.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\FrameworkBundle\Unit\Component\Doctrine\__fixtures;
+
+use Doctrine\ORM\Mapping as ORM;
+use Shopsys\FrameworkBundle\Model\Product\Pricing\ProductPriceRecalculationScheduler;
+use Shopsys\FrameworkBundle\Model\Product\Product as BaseProduct;
+use Shopsys\FrameworkBundle\Model\Product\ProductCategoryDomainFactoryInterface;
+use Shopsys\FrameworkBundle\Model\Product\ProductData as BaseProductData;
+
+/**
+ * @ORM\Table(name="products")
+ * @ORM\Entity
+ */
+class Product extends BaseProduct
+{
+    /**
+     * @param \Shopsys\ShopBundle\Model\Product\ProductData $productData
+     * @param \Shopsys\FrameworkBundle\Model\Product\ProductCategoryDomainFactoryInterface $productCategoryDomainFactory
+     * @param \Shopsys\ShopBundle\Model\Product\Product[]|null $variants
+     */
+    protected function __construct(BaseProductData $productData, ProductCategoryDomainFactoryInterface $productCategoryDomainFactory, array $variants = null)
+    {
+        parent::__construct($productData, $productCategoryDomainFactory, $variants);
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Product\ProductCategoryDomainFactoryInterface $productCategoryDomainFactory
+     * @param \Shopsys\ShopBundle\Model\Product\ProductData $productData
+     * @param \Shopsys\FrameworkBundle\Model\Product\Pricing\ProductPriceRecalculationScheduler $productPriceRecalculationScheduler
+     */
+    public function edit(
+        ProductCategoryDomainFactoryInterface $productCategoryDomainFactory,
+        BaseProductData $productData,
+        ProductPriceRecalculationScheduler $productPriceRecalculationScheduler
+    ) {
+        parent::edit($productCategoryDomainFactory, $productData, $productPriceRecalculationScheduler);
+    }
+}
+
